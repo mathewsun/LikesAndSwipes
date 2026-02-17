13d1e05151bd61f7de14c4373bbb6b42fe7e3ec2
mathewsun <mathew.dubrovin@gmail.com>
2026-02-17 20:21:32 +0300

    create migration AddCreatedFieldToAspNetUsersTable

---
+++ LikesAndSwipes/Migrations/20260217171203_AddCreatedFieldToAspNetUsersTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LikesAndSwipes.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedFieldToAspNetUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true,
+                defaultValueSql: "now()");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+    }
+}
