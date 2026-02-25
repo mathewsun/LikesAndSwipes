5edef89d2fd885fd33b45c3aed66173e808ab03d
mathewsun <mathew.dubrovin@gmail.com>
2026-02-25 15:44:31 +0300

    add User fileds FirstName, Age, Sex, Birthday

---
+++ LikesAndSwipes/Migrations/20260225124019_AddUserFirstNameSexBirthDay.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LikesAndSwipes.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserFirstNameSexBirthDay : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDay",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBisexual",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHomosexual",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Sex",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDay",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsBisexual",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsHomosexual",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Sex",
+                table: "AspNetUsers");
+        }
+    }
+}
--- LikesAndSwipes/Models/User.cs
+++ LikesAndSwipes/Models/User.cs
@@ -5,5 +5,17 @@ namespace LikesAndSwipes.Models
     public class User : IdentityUser
     {
         public DateTime Created { get; set; }
+
+        public string FirstName { get; set; }
+
+        public bool Sex { get; set; } //0 - man, 1 - woman
+
+        public bool IsHomosexual { get; set; }
+
+        public bool IsBisexual { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime BirthDay { get; set; }
     }
 }
