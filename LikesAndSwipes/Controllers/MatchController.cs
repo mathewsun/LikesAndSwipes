8ca5b30c6f3eba7ceec658ffef0083199b3b1669
mathewsun <mathew.dubrovin@gmail.com>
2026-03-10 13:30:38 +0300

    add match page

---
+++ LikesAndSwipes/Controllers/MatchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LikesAndSwipes.Controllers
+{
+    public class MatchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
