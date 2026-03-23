e9402f5335bb318927dc3782c65f8ff2a7796b26
mathewsun <mathew.dubrovin@gmail.com>
2026-03-24 01:02:14 +0300

    upd links userprofile

---
+++ LikesAndSwipes/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LikesAndSwipes.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet("user")]
+        public IActionResult User(string id)
+        {
+            return View();
+        }
+
+        [HttpGet("user/{id}")]
+        public IActionResult Index(string id)
+        {
+            return View();
+        }
+    }
+}
