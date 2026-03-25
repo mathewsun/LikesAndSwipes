d855a9e5b5538f7e5c24277034f8e75e6ca35d86
mathewsun <mathew.dubrovin@gmail.com>
2026-03-26 01:04:39 +0300

    upd UserController

--- LikesAndSwipes/Controllers/UserController.cs
+++ LikesAndSwipes/Controllers/UserController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LikesAndSwipes.Models;
+using LikesAndSwipes.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LikesAndSwipes.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private DataRepository _dataRepository;
+
+        public UserController(
+            UserManager<User> userManager,
+            DataRepository dataRepository)
+        {
+
+            _dataRepository = dataRepository;
+        }
+
         [HttpGet("user")]
         public IActionResult User(string id)
         {
@@ -13,6 +27,8 @@ namespace LikesAndSwipes.Controllers
         [HttpGet("user/{id}")]
         public IActionResult Index(string id)
         {
+
+
             return View();
         }
     }
