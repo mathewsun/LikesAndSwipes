c8c9b8ab246a73c13ea47c78c712e3ead66e9318
Dev Space <mathewsun@rambler.ru>
2026-03-26 01:15:42 +0300

    Add responsive photo grid on user profile page

---
+++ LikesAndSwipes/Models/UserProfilePhotosViewModel.cs
@@ -0,0 +1,9 @@
+namespace LikesAndSwipes.Models
+{
+    public class UserProfilePhotosViewModel
+    {
+        public string UserId { get; set; } = string.Empty;
+
+        public List<UserPhoto> Photos { get; set; } = new();
+    }
+}
