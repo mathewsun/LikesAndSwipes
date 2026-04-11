1592d5f28ef52f0bd12f1b696fe83d4c63a8e816
mathewsun <mathew.dubrovin@gmail.com>
2026-04-12 00:28:03 +0300

    upd match contrller

--- LikesAndSwipes/Controllers/MatchController.cs
+++ LikesAndSwipes/Controllers/MatchController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LikesAndSwipes.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LikesAndSwipes.Controllers
 {
     public class MatchController : Controller
     {
+        private DataRepository _dataRepository;
+
+        public MatchController(DataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+
         public IActionResult Index()
         {
+
+
             return View();
         }
     }
