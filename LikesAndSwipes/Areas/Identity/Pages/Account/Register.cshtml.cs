b3ad7044e2e5dd52b7edf3d346f6d8585de3e574
mathewsun <mathew.dubrovin@gmail.com>
2026-04-25 00:14:49 +0300

    fix RecaptchaVerifyResponse

--- LikesAndSwipes/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LikesAndSwipes/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -390,12 +390,16 @@ namespace LikesAndSwipes.Areas.Identity.Pages.Account
             var responseJson = await response.Content.ReadAsStringAsync();
             var recaptchaResponse = JsonSerializer.Deserialize<RecaptchaVerifyResponse>(responseJson);
 
-            return recaptchaResponse?.Success ?? false;
+            return recaptchaResponse?.success ?? false;
         }
 
         private sealed class RecaptchaVerifyResponse
         {
-            public bool Success { get; set; }
+            public bool success { get; set; }
+            public DateTime challenge_ts { get; set; }
+            public string hostname { get; set; }
+            public float score { get; set; }
+            public string action { get; set; }
         }
 
         private IUserEmailStore<User> GetEmailStore()
